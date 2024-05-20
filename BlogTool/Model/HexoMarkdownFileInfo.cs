c32122539738b85943d3a74c14b248e5bed6fcf4
小 林 <jevonsflash@hotmail.com>
2024-05-20 11:57:59 +0800

    update

--- BlogTool/Model/HexoMarkdownFileInfo.cs
+++ BlogTool/Model/HexoMarkdownFileInfo.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using YamlDotNet.Serialization;
 using BlogTool.Core.Markdown;
+using System.ComponentModel;
 
 namespace BlogTool.Model
 {
@@ -12,24 +13,28 @@ namespace BlogTool.Model
     {
         public List<string> Categories { get; set; } = new List<string>();
 
+        [DisplayName("标题")]
         public string Title
         {
             get;
             set;
         }
 
+        [DisplayName("关键字")]
         public string Keywords
         {
             get;
             set;
         }
 
+        [DisplayName("连接")]
         public string Link
         {
             get;
             set;
         }
 
+        [DisplayName("创建时间")]
         public DateTime? DateCreated
         {
             get;
@@ -37,11 +42,13 @@ namespace BlogTool.Model
         }
 
       
+        [DisplayName("文件路径")]
         public string FilePath
         {
             get;
             set;
         }
+        [DisplayName("内容")]
         public string Description {get; set; }  
     }
 
